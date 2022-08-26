8635986f75caf1117b7e77acb64e8ba39c1b04b8
shivs2000 <singhshivam0666@gmail.com>
2022-08-26 22:30:56 +0530

    added quill with basic config

--- src/App.js
+++ src/App.js
@@ -1,25 +1,51 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import ReactQuill from "react-quill";
+import "react-quill/dist/quill.snow.css";
+import React, { useState } from "react";
 
 function App() {
+  const [value, setValue] = useState("");
+  const handleChange = (e) => {
+    setValue(e);
+  };
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <ReactQuill
+        theme="snow"
+        modules={App.modules}
+        formats={App.formats}
+        onChange={handleChange}
+        value={value}
+      />
     </div>
   );
 }
+App.modules = {
+  toolbar: [
+    [{ header: [1, 2, false] }],
+    ["bold", "italic", "underline", "strike", "blockquote"],
+    [
+      { list: "ordered" },
+      { list: "bullet" },
+      { indent: "-1" },
+      { indent: "+1" },
+    ],
+    ["link", "image"],
+    ["clean"],
+  ],
+};
+App.formats = [
+  "header",
+  "bold",
+  "italic",
+  "underline",
+  "strike",
+  "blockquote",
+  "list",
+  "bullet",
+  "indent",
+  "link",
+  "image",
+];
 
 export default App;
